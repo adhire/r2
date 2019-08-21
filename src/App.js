da741bf3d551231735fd5dba94e5fe642b527706
Adam <adamdhire@gmail.com>
2019-08-21 10:59:46 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Profile from "./components/profile/profile.component";
+import EditProfile from "./components/edit-profile/edit-profile.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/profile/'>Profile</Link>
+            </li>
+            <li>
+              <Link to='/edit/'>Edit</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path='/' exact component={Profile} />
+        <Route path='/profile/' component={Profile} />
+        <Route path='/edit/' component={EditProfile} />
+      </div>
+    </Router>
   );
 }
 
